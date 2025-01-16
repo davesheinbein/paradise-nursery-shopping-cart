541f7a617c4107354d65d6afa122a74af6a02658
David Sheinbein <davidsheinbeindev@gmail.com>
2025-01-16 13:04:27 -0800

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,50 @@
-import { useState } from 'react';
-import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
-	Link,
-} from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import ProductListingPage from './pages/ProductListingPage';
-import ShoppingCartPage from './pages/ShoppingCartPage';
+import React, { useState } from 'react';
+import ProductList from './pages/ProductList';
 import './App.css';
+import AboutUs from './pages/AboutUs';
 
 function App() {
-	const [cart, setCart] = useState([]);
+	const [showProductList, setShowProductList] =
+		useState(false);
 
-	const addToCart = (plant) => {
-		setCart([...cart, plant]);
-	};
-
-	const removeFromCart = (plant) => {
-		setCart(cart.filter((item) => item.id !== plant.id));
-	};
-
-	const updateCartQuantity = (plant, quantity) => {
-		setCart(
-			cart.map((item) =>
-				item.id === plant.id ? { ...item, quantity } : item
-			)
-		);
+	const handleGetStartedClick = () => {
+		setShowProductList(true);
 	};
 
 	return (
-		<Router>
-			<header>
-				<nav>
-					<Link to='/'>Home</Link>
-					<Link to='/products'>Products</Link>
-					<Link to='/cart'>Cart ({cart.length})</Link>
-				</nav>
-			</header>
-			<Routes>
-				<Route path='/' element={<LandingPage />} />
-				<Route
-					path='/products'
-					element={
-						<ProductListingPage addToCart={addToCart} />
-					}
-				/>
-				<Route
-					path='/cart'
-					element={
-						<ShoppingCartPage
-							cart={cart}
-							removeFromCart={removeFromCart}
-							updateCartQuantity={updateCartQuantity}
-						/>
-					}
-				/>
-			</Routes>
-		</Router>
+		<div className='app-container'>
+			<div
+				className={`landing-page ${
+					showProductList ? 'fade-out' : ''
+				}`}
+			>
+				<div className='background-image'></div>
+				<div className='content'>
+					<div className='landing_content'>
+						<h1>Welcome To Paradise Nursery</h1>
+						<div className='divider'></div>
+						<p>Where Green Meets Serenity</p>
+
+						<button
+							className='get-started-button'
+							onClick={handleGetStartedClick}
+						>
+							Get Started
+						</button>
+					</div>
+					<div className='aboutus_container'>
+						<AboutUs />
+					</div>
+				</div>
+			</div>
+			<div
+				className={`product-list-container ${
+					showProductList ? 'visible' : ''
+				}`}
+			>
+				<ProductList />
+			</div>
+		</div>
 	);
 }
 
